89be522af0832c289ef70ee7e83c161c7695620b
Claudiu Mera <claudiu.mera@live.com>
2017-09-03 15:36:24 +0300

    First version of HotelBot

---
+++ HotelBot/HotelBot/Dialogs/GreetingDialog.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.Bot.Connector;
+
+namespace HotelBot.Dialogs
+{
+    [Serializable]
+    public class GreetingDialog : IDialog
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            await context.PostAsync("Hi, I'm John Bot");
+            await Respond(context);
+
+            context.Wait(MessageReceivedAsync);
+        }
+
+        public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            var message = await argument;
+            var userName = string.Empty;
+            var getName = false;
+
+            context.UserData.TryGetValue<string>("Name", out userName);
+            context.UserData.TryGetValue<bool>("GetName", out getName);
+
+            if(getName)
+            {
+                userName = message.Text;
+
+                context.UserData.SetValue<string>("Name", userName);
+                context.UserData.SetValue<bool>("GetName", false);
+            }
+
+            await Respond(context);
+            context.Done(message);
+        }
+
+        private static async Task Respond(IDialogContext context)
+        {
+            var userName = string.Empty;
+            context.UserData.TryGetValue<string>("Name", out userName);
+
+            if (string.IsNullOrEmpty(userName))
+            {
+                await context.PostAsync("What is your name?");
+                context.UserData.SetValue<bool>("GetName", true);
+            }
+            else
+            {
+                await context.PostAsync($"Hi, {userName}. How can I help you with?");
+            }
+        }
+    }
+}
---
+++ HotelBot/HotelBot/Dialogs/GreetingDialogBasic.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.Bot.Connector;
+
+namespace HotelBot.Dialogs
+{
+    [Serializable]
+    public class GreetingDialogBasic : IDialog
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            await context.PostAsync("Hi, I'm John Bot");
+            context.Wait(MessageReceivedAsync);
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            var message = await argument;
+            var userName = string.Empty;
+            var getName = false;
+
+            context.UserData.TryGetValue<string>("Name", out userName);
+            context.UserData.TryGetValue<bool>("GetName", out getName);
+
+            if(getName)
+            {
+                userName = message.Text;
+
+                context.UserData.SetValue<string>("Name", userName);
+                context.UserData.SetValue<bool>("GetName", false);
+            }
+
+            if(string.IsNullOrEmpty(userName))
+            {
+                await context.PostAsync("What is your name?");
+                context.UserData.SetValue<bool>("GetName", true);
+            }
+            else
+            {
+                await context.PostAsync($"Hi, {userName}. How can I help you with?");
+            }
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
